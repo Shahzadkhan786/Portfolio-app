1b33af737cb6f4bf3e886d9f1ce1db93ca32d251
Shahzad Akhtar Khan <shahzadakj1778@gmail.com>
2023-07-05 17:11:53 +0500

    added functions

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ const AboutMePage = () => {
 						height={640}
 						aria-label={"image of WhatsApp Image 2023-05-28 at 12.53.47 PM.jpeg"}
 						alt={"WhatsApp Image 2023-05-28 at 12.53.47 PM.jpeg"}
-						src={"/images/123tre.png"}
+						src={"/images/12tre.png"}
 						className='profileImage'
 					/>
 				</div>
