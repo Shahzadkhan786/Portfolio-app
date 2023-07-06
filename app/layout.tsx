09866d3963d52dd6615dbf95514dc5991c8154d5
Shahzad Akhtar Khan <shahzadakj1778@gmail.com>
2023-07-06 11:56:18 +0500

    Revert "first commit"
    This reverts commit 1d7547d8471d82523c224490cefa5720c85b6d2b.
    
    # Conflicts:
    #	app/data/data.tsx
    #	app/my-projects/components/project-card.tsx
    #	app/my-projects/page.tsx
    #	app/page.tsx
    #	package-lock.json
    #	package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import "./globals.css";
+import React from "react"
 import  Footer  from "./components/footer";
 import  Header  from "./components/header";
-import { aboutMeData, pages, contactMeLinks } from "./data/data";
+import { aboutMeData, pages} from "./data/data";
 export default function RootLayout({
 	children,
 }: {
@@ -14,7 +15,7 @@ export default function RootLayout({
 			<body>
 				<Header name={aboutMeData.name} pages={pages} />
 				<main className='sm:mb-32 mb-16'>{children}</main>
-				<Footer contactMeLinks={contactMeLinks} />
+				<Footer />
 			</body>
 		</html>
 	);
