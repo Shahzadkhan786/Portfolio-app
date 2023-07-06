8e783d51997de2ae1bd023c927b88ef5ceff4ce3
Shahzad Akhtar Khan <shahzadakj1778@gmail.com>
2023-07-06 11:55:57 +0500

    Revert "first commit"
    This reverts commit 1d7547d8471d82523c224490cefa5720c85b6d2b.
    
    # Conflicts:
    #	app/data/data.tsx
    #	app/my-projects/components/project-card.tsx
    #	app/my-projects/page.tsx
    #	app/page.tsx
    #	package-lock.json
    #	package.json

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,11 @@ import { aboutMeData } from "./data/data";
 const AboutMePage = () => {
 	return (
 		<div className='flex flex-col items-center'>
+			
 			<section className='flex sm:flex-row sm:w-10/12 items-center flex-col w-full'>
+				
 				<div className=' fixed-w w-full sm:w-1/3-g flex justify-center mt-12 rounded-full border-2'>
+					
 					<Image
 						priority={false}
 						width={480}
@@ -16,19 +19,27 @@ const AboutMePage = () => {
 						src={"/images/12tre.png"}
 						className='profileImage'
 					/>
+
 				</div>
+
 				<div className='sm:ml-20 sm:mt-0  sm:w-2/3 w-full mt-6 ml-0 p-10'>
+					
 					<h2 className=''> {aboutMeData.title}</h2>
 					<p className='text-base'> {aboutMeData.body}</p>
 				</div>
+
 			</section>
-			<section className='bg-orange w-full p-4 text-center h-48'>
-				 <b>{aboutMeData.highlightedBody}</b>
+
+			<section className='bg-orange w-full p-4 text-center h-48'>	
+		    {aboutMeData.highlightedBody}
 			</section>
+
 			<section className='flex sm:flex-row sm:w-10/12 p-10 items-center w-full flex-col'>
+				
 				<div className='flex flex-col  w-full mb-10 sm:mb-0 sm:w-7/12'>
-					 {aboutMeData.body2}
-				</div>
+                 {aboutMeData.body2}
+                </div>
+
 				<div className='sm:w-5/12 sm:ml-10 w-full ml-0 border-4'>
 					<Image
 						priority={false}
@@ -40,7 +51,9 @@ const AboutMePage = () => {
 						className='h-250 object-cover w-500'
 					/>
 				</div>
+
 			</section>
+
 		</div>
 	);
 };
