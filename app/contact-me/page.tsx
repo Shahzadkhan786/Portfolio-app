4245da106d2754b9f799c0f33d92e1be9cea9a8c
Shahzad Akhtar Khan <shahzadakj1778@gmail.com>
2023-07-06 18:54:09 +0500

    Revert "first commit"
    This reverts commit 1d7547d8471d82523c224490cefa5720c85b6d2b.
    
    # Conflicts:
    #	app/data/data.tsx
    #	app/my-projects/components/project-card.tsx
    #	app/my-projects/page.tsx
    #	app/page.tsx
    #	package-lock.json
    #	package.json

--- app/contact-me/page.tsx
+++ app/contact-me/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { contactMeText } from '../data/data'
 import { Form } from './components/form'
-import { Mail, Phone } from 'lucide-react'
+import { Mail } from 'lucide-react'
 
 
 const ContactMePage= () => {
@@ -12,10 +12,7 @@ const ContactMePage= () => {
               <div className=" flex flex-col w-2/4">
                     <h3 className='py-4 text-[24px] '><b>{contactMeText.title}</b></h3>
                     <p>{contactMeText.body}</p>
-                <div className="pt-8 absolute bottom-20 ">
-                    <p><Mail className="inline-flex"/>shahzadaktarkhanjadoon567@gmail.com</p>
-                    <p><Phone className="inline-flex"/>+92340 7145223</p>
-                </div>
+                
               </div >
               <div className="sm:place-items-end w-2/4"><Form /></div>
             </div>
