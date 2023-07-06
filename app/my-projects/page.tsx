0890a00415201b3007619012def35f990ac98e0f
Shahzad Akhtar Khan <shahzadakj1778@gmail.com>
2023-07-06 11:57:10 +0500

    Revert "first commit"
    This reverts commit 1d7547d8471d82523c224490cefa5720c85b6d2b.
    
    # Conflicts:
    #	app/data/data.tsx
    #	app/my-projects/components/project-card.tsx
    #	app/my-projects/page.tsx
    #	app/page.tsx
    #	package-lock.json
    #	package.json

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -8,7 +8,7 @@ function MyProjects(){
             <h2 className="text-2xl font-bold mb-4">My Projects:</h2>
             <div className="flex flex-row flex-wrap gap-10 mt-6">
                 {projects.map((project) =>{
-                    return <ProjectCard data={project} />
+                    return <ProjectCard key={project.id} data={project} />
                     
                 })}
             </div>
